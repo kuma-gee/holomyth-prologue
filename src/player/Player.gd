13132850b1eef7aa297603ae2020cf07a63f75dd
myin <minyin142p@gmail.com>
2023-01-29 11:08:42 +0100

    add world

--- src/player/Player.gd
+++ src/player/Player.gd
@@ -3,14 +3,18 @@ extends CharacterBody2D
 @export var speed := 300.0
 
 @onready var input: PlayerInput = $PlayerInput
+@onready var sprite := $Sprite2D
 
 # Get the gravity from the project settings to be synced with RigidBody nodes.
-var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
+var gravity = ProjectSettings.get_setting("physics/2d/default_gravity") * 10
 
 
 func _physics_process(delta):
 	var motion = input.get_action_strength("move_right") - input.get_action_strength("move_left")
 	velocity.x = motion * speed
+	
+	if abs(motion) > 0:
+		sprite.flip_h = sign(motion) == -1
 	
 	if not is_on_floor():
 		velocity.y += gravity * delta
